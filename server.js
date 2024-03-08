f15960b839c8e09968726d6d345fe1de5706a8d9
Manoj Kumar N <manojcsengineer@gmail.com>
2024-03-08 17:56:10 +0530

    message-sending-and-receiving-api-woring-perfectly

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connectDb = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
